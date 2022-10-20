c733da8e424960c26960e4678d5be99a66759b2d
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-10-20 09:03:08 -0300

    creation de la classe et ajout des libraires

---
+++ src/PlusGrandeValeur.java
@@ -0,0 +1,14 @@
+/*
+  Titre      : plus grande valeur
+  Auteur     : Duvalier Tsagmo
+  Date       : 20/10/2022
+  Description: ce programme doit afficher la plus grande valeur et son index
+  Version    : 0.0.1
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+public class PlusGrandeValeur {
+    public static void main(String[] args) {
+       
+}
